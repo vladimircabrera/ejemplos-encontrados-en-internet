20832736324061d316e0e6b0006547881c7c8ba2
vladimircabrera <vladimir.cv.21@gmail.com>
2017-04-27 22:26:05 -0300

    Update ejemplo1.c

--- ejemplo1.c
+++ ejemplo1.c
@@ -1,30 +1,86 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ncurses.h>
-char pensar(); //Funcion
-       char almacen[20]; //Palabras-letras (char)
-       int resul; //Utilizamos int porque usaremos numeros (int)
+# include <stdio.h>
+# include <stdlib.h>
+# include <math.h>//Contiene la funcion sqrt
+
+int srmd();
+int ecuac();
+
+int elegir;
+int resul = '0';//Se indica el valor a la variable
+int valor1 = '0';
+int valor2 = '0';
+
+int main (void) 
+{
+    printf ("1)Operaciones basicas\n2)Ecuaciones: 2do grado (coeficientes)\n3)Salir");
+    printf ("\n\n>");
+    scanf ("%s", &elegir);
        
-main() {
-       system("cls"); //Limpia pantalla
-       printf("1)Guardar dato\n2)Leer dato\n3)Salir"); //Escribe caracteres
-       printf("\n\n>");
-      scanf("%s",&resul);//Guarda en una variable
-      
-       if (resul =='1') {//Se abre parentesis cuando la sentencia IF tiene mas de una linea
-          printf("\nIntroduce el dato>");
-          scanf("%s",&almacen);}//Y luego se cierra en la ultima linea
-               
-       if (resul =='2')//Aqui no hace falta "{" porque solo usa una linea
-          pensar();//1 linea
+    if (elegir == '1')
+       srmd();
+    if (elegir == '2')
+       ecuac();
+    if (elegir == '3')
+       exit(0);
+    system("cls");
+    main(); 
+}
+
+int srmd(void) 
+{
+    system ("cls");
+    printf ("1)Sumar\n2)Restar\n3)Multiplicar\n4)Dividir");
+    printf ("\n\n>");
+    scanf ("%s", &elegir);
+    
+    system ("cls");
        
-       if (resul =='3')
-          exit(0); //Salir
+    printf("Escribe el valor 1:\n\n\n>");           
+    scanf("%d", &valor1);
+    printf("Escribe el valor 2:\n\n\n>");          
+    scanf("%d", &valor2);
+                           
+    if (elegir == '1'){
+       resul = valor1 + valor2;
+       printf("%d + %d = %d\n\n", valor1, valor2, resul);
+    }
+    if (elegir == '2'){
+       resul = valor1 - valor2;
+       printf("%d - %d = %d\n\n", valor1, valor2, resul);
+    }
+    if (elegir == '3'){
+       resul = valor1 * valor2;
+       printf("%d * %d = %d\n\n", valor1, valor2, resul);
+    }
+    if (elegir == '4'){
+       resul = valor1 / valor2;
+       printf("%d / %d = %d\n\n", valor1, valor2, resul);
+    }
+                
+    system("pause");                      
+}
+                           
+                           
+int ecuac(void)
+{//operaciones para resolver ecuacion de segundo grado
+    double a, b, c, d, x1, x2;
        
-       main(); //Llama a main de nuevo(vuelve al inicio)
-                }
-                     
-char pensar(){//En las funciones siempre abrir los "{" "}"
-             printf("Datos: %s\n\n",&almacen);
-             system("pause");
-             }
+    system ("cls");
+       
+    printf ("Introducir los valores de a, b y c separados con un espacio");
+    printf ("\n\n>");
+    scanf ("%lf %lf %lf", &a, &b, &c);
+       
+    de = b * b - 4 * a * c;
+       
+    if (d < 0)
+       printf("No se puede resolver, las raices son complejas\n"); //la  ecuacion debe dar 0, sino no funciona (Xcuadrado(A) - 2X(B) -3(C) =0  funciona porque x=3)
+    //calculando
+    de = sqrt(d);
+    x1 = (-b +d) / (2 * a);
+    x2 = (-b -d) / (2 * a);
+    
+    printf ("x1= %g\nx2= %g\n\n", x1, x2);
+       
+    system("pause"); 
+}
